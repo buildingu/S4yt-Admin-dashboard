c918d51cef0138fc7584684eb86c491937f3c906
Sidragon123 <161826426+Sidragon123@users.noreply.github.com>
2025-02-04 18:00:24 +0800

    rafflePageAdminPanel part 1

---
+++ src/customComponents/RaffleItem.jsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+
+export default function RaffleItem({
+  item,
+  description,
+  quantity,
+  resourceLink,
+}) {
+  return (
+    <Card className="bg-[#333] border border-gray-700 focus:border-[#F9EB02] text-white w-96 mr-6 ml-6 text-center">
+      <CardTitle className="text-2xl mr-6 ml-6 mb-6 mt-6">{item}</CardTitle>
+      <CardContent>
+        <div className="grid grid-cols-3 grid-rows-3 mr-6 ml-6 gap-4 text-center align-middles">
+          <div className="row-span-2">img</div>
+          <div className="col-span-2 row-span-2">{description}</div>
+          <div>Quantity: {quantity}</div>
+          <div className="col-span-2">
+            <a href={resourceLink} className="text-center">
+              Resource Link
+            </a>
+          </div>
+        </div>
+      </CardContent>{" "}
+      <CardFooter className="flex justify-center">
+        <Button id="delete" onClick="#">
+          Delete
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/pages/RafflePartenerAdminPanel.jsx
+++ src/pages/RafflePartenerAdminPanel.jsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import React from "react";
+import RaffleItem from "@/customComponents/RaffleItem";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
+function AddRaffleItem({ className, ...props }) {
+  let [item, setItem] = useState("");
+  let [description, setDescription] = useState("");
+  let [quantity, setQuantity] = useState(0);
+  let [resourceLink, setResourceLink] = useState("");
+  function handleItemChange(e) {
+    set(e.target.value);
+  }
+  function handleDescriptionChange(e) {
+    set(e.target.value);
+  }
+  function handleQuantityChange(e) {
+    set(e.target.value);
+  }
+  function handleResourceLinkChange(e) {
+    set(e.target.value);
+  }
+  function handleSubmit() {
+    let formData = {
+      item: item,
+      description: description,
+      quantity: quantity,
+      resourceLink: resourceLink,
+    };
+    //send this to db
+  }
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="bg-[#333] border border-gray-700 focus:border-[#F9EB02] text-white mb-4">
+        <CardHeader>
+          <CardTitle className="text-2xl">New Item</CardTitle>
+          <CardDescription className="text-gray-400">
+            Create New Item for Raffle
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="item">Item</Label>
+                <Input
+                  className="text-white-400"
+                  id="item"
+                  type="text"
+                  placeholder="Item Name"
+                  required
+                  onChange={handleItemChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="description"></Label>
+                <Input
+                  className="text-white-400"
+                  id="description"
+                  type="textarea"
+                  placeholder="Item Description"
+                  required
+                  onChange={handleDescriptionChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="quantity"></Label>
+                <Input
+                  className="text-white-400"
+                  id="quantity"
+                  type="number"
+                  placeholder="Item Quantity"
+                  required
+                  onChange={handleQuantityChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="resourceLink"></Label>
+                <Input
+                  className="text-white-400"
+                  id="resourceLink"
+                  type="url"
+                  placeholder="Resource Link"
+                  required
+                  onChange={handleResourceLinkChange}
+                />
+              </div>
+
+              <Button type="submit" className="w-full" onClick={handleSubmit}>
+                Create
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
 function RafflePartenerAdminPanel() {
   return (
-    <div><h1>RafflePartenerAdminPanel</h1></div>
-  )
+    <div>
+      <AddRaffleItem></AddRaffleItem>
+      {/* do a forEach for raffleItem from the db data to render out as these blocks */}
+      {/* <RaffleItem item="placeholderItem" description="placeholderDescription" /> */}
+    </div>
+  );
 }
 
-export default RafflePartenerAdminPanel
+export default RafflePartenerAdminPanel;
