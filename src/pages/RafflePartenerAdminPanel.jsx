94c6add22f7ab89fe74b3c000e538276bc77268b
Sidragon123 <161826426+Sidragon123@users.noreply.github.com>
2025-02-05 17:32:57 +0800

    Raffle Partner Admin Panel

--- src/pages/RafflePartenerAdminPanel.jsx
+++ src/pages/RafflePartenerAdminPanel.jsx
@@ -11,7 +11,79 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+function EditYourInfo({ className, ...props }) {
+  // let [initialName, initialDescription] = await useEffect(async () => {
+  //   let iName = await fetch("#");
+  //   let iDescription = await fetch("#");
+  //   let iNameJson = await iName.json();
+  //   let iDescriptionJson = await iDescription.json();
+  //   return [iNameJson.toString(), iDescriptionJson.toString()];
+  // });
+  let [name, setName] = useState("");
+  let [description, setDescription] = useState("");
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+  }
+  function handleSubmit() {
+    return;
+  }
+  return (
+    <div
+      className={cn("flex flex-col gap-6 border-transparent", className)}
+      {...props}
+    >
+      <Card className="bg-[#333] text-white mb-4 border-transparent">
+        <CardHeader>
+          <CardTitle className="text-2xl">
+            Update information about you
+          </CardTitle>
+          <CardDescription className="text-gray-400">
+            Contact us to update your logo!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="item">Name</Label>
+                <Input
+                  className="text-white-400"
+                  id="item"
+                  type="text"
+                  placeholder="Your Business Name"
+                  required
+                  onChange={handleNameChange}
+                  // defaultValue={initialName}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="description">Description</Label>
+                <Input
+                  className="text-white-400"
+                  id="description"
+                  type="textarea"
+                  placeholder="Your Business Description"
+                  required
+                  onChange={handleDescriptionChange}
+                  // defaultValue={initialDescription}
+                />
+              </div>
+              <Button type="submit" className="w-full" onClick={handleSubmit}>
+                Create
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
 
 function AddRaffleItem({ className, ...props }) {
   let [item, setItem] = useState("");
@@ -19,16 +91,16 @@ function AddRaffleItem({ className, ...props }) {
   let [quantity, setQuantity] = useState(0);
   let [resourceLink, setResourceLink] = useState("");
   function handleItemChange(e) {
-    set(e.target.value);
+    setItem(e.target.value);
   }
   function handleDescriptionChange(e) {
-    set(e.target.value);
+    setDescription(e.target.value);
   }
   function handleQuantityChange(e) {
-    set(e.target.value);
+    setQuantity(e.target.value);
   }
   function handleResourceLinkChange(e) {
-    set(e.target.value);
+    setResourceLink(e.target.value);
   }
   function handleSubmit() {
     let formData = {
@@ -40,8 +112,11 @@ function AddRaffleItem({ className, ...props }) {
     //send this to db
   }
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="bg-[#333] border border-gray-700 focus:border-[#F9EB02] text-white mb-4">
+    <div
+      className={cn("flex flex-col gap-6 border-transparent", className)}
+      {...props}
+    >
+      <Card className="bg-[#333] text-white mb-4 border-transparent">
         <CardHeader>
           <CardTitle className="text-2xl">New Item</CardTitle>
           <CardDescription className="text-gray-400">
@@ -109,9 +184,33 @@ function AddRaffleItem({ className, ...props }) {
 function RafflePartenerAdminPanel() {
   return (
     <div>
-      <AddRaffleItem></AddRaffleItem>
-      {/* do a forEach for raffleItem from the db data to render out as these blocks */}
-      {/* <RaffleItem item="placeholderItem" description="placeholderDescription" /> */}
+      <Tabs
+        defaultValue="you"
+        className="w-[400px] bg-[#333] border-transparent focus:border-[#F9EB02] text-white mb-4 rounded-xl"
+      >
+        <TabsList className="grid w-full grid-cols-2 bg-[#333] border border-gray-700 focus:border-[#F9EB02] text-white mb-4">
+          <TabsTrigger
+            value="you"
+            className="bg-[#333] focus:border-[#F9EB02] text-white mb-4"
+          >
+            You
+          </TabsTrigger>
+          <TabsTrigger
+            value="raffleItems"
+            className="bg-[#333] focus:border-[#F9EB02] text-white mb-4"
+          >
+            Raflle Items
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="you">
+          <EditYourInfo />
+        </TabsContent>
+        <TabsContent value="raffleItems">
+          <AddRaffleItem />
+          {/* do a forEach for raffleItem from the db data to render out as these blocks */}
+          {/* <RaffleItem item="placeholderItem" description="placeholderDescription" /> */}
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
