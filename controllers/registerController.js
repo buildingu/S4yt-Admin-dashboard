7c5282c10b0a4eb82bbd86abc1edc10335bafa93
shema <dabirishema@gmail.com>
2025-02-11 11:13:04 -0600

    removed log

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,8 +6,6 @@ const bcrypt = require('bcrypt');
 exports.registerUser = async (req, res) => {
   try {
     const {businessName, email, password } = req.body;
-    console.log(req.body
-    );
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
