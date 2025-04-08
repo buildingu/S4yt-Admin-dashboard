7e73db1c1b2ff4b882e92736596dfccdfa9d687f
shema <dabirishema@gmail.com>
2025-04-08 17:33:28 -0600

    no more forgot pswd

--- views/src/pages/Login.jsx
+++ views/src/pages/Login.jsx
@@ -99,12 +99,12 @@ function Login({ className, ...props }) {
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <a
+                 {/* <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
                     Forgot your password?
-                  </a>
+                  </a>*/}
                 </div>
                 <Input
                   id="password"
