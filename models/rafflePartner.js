948636ba6cff6830253f1fb943e4f9814a4d63b2
shema <dabirishema@gmail.com>
2025-04-11 15:20:34 -0600

    partner updates

--- models/rafflePartner.js
+++ models/rafflePartner.js
@@ -1,7 +1,11 @@
 const { Schema, mongoose } = require("mongoose");
 
 const rafflePartnerSchema = new Schema({
-  logo: { type: String, required: true },
+  logo: { type: String, required: true }, //program logo
+  resource_logo: String,
+  resource_name: String,
+  description: String,
+  name: String,
   organization_name: { type: String, required: true },
   resource_link: { type: String, required: true },
   resource_category: { type: String, required: true },
