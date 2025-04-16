2bd79b339245667187f79b1216797149c1b7ef54
Sidragon123 <161826426+Sidragon123@users.noreply.github.com>
2025-04-16 17:53:26 +0800

    commit

--- controllers/raffleItemController.js
+++ controllers/raffleItemController.js
@@ -3,19 +3,51 @@ const RafflePartner = require("../models/rafflePartner");
 const RaffleItem = require("../models/raffleItem");
 
 const createRaffleItem = async (req, res) => {
-  const { item, description, quantity, logo, resourceLink, rafflePartner } =
-    req.body;
+  const { item, description, quantity, resourceLink, rafflePartner } = req.body;
+  const logo = req.files?.logo;
   try {
     const associatedRafflePartner = await RafflePartner.findOne({
       organization_name: rafflePartner,
     });
+    let logoUrl = null;
+
+    console.log("Cloudinary config:", {
+      name: process.env.CLOUDINARY_CLOUD_NAME,
+      key: process.env.CLOUDINARY_API_KEY,
+      secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    if (logo) {
+      if (businessExists.logo) {
+        const logoUrlParts = businessExists.logo.split("/");
+        const publicIdWithExt = logoUrlParts.slice(-1)[0];
+        const folder = logoUrlParts[logoUrlParts.length - 2];
+        const publicId = `${folder}/${publicIdWithExt.split(".")[0]}`;
+
+        try {
+          await cloudinary.uploader.destroy(publicId);
+          console.log(`Deleted previous logo: ${publicId}`);
+        } catch (err) {
+          console.warn(`Failed to delete previous logo from Cloudinary:`, err);
+        }
+      }
+
+      const dataUri = `data:${logo.mimetype};base64,${logo.data.toString(
+        "base64"
+      )}`;
+      const uploadResult = await cloudinary.uploader.upload(dataUri, {
+        folder: "business_logos",
+        resource_type: "auto",
+      });
+      logoUrl = uploadResult.secure_url;
+    }
 
     const raffleItem = new RaffleItem({
       raffle_partner: associatedRafflePartner._id,
       raffle_partner_name: rafflePartner,
       name: item,
       description: description,
-      image_src: logo,
+      image_src: logoUrl,
       stock: quantity,
       resource_link: resourceLink,
     });
@@ -32,11 +64,44 @@ const updateRaffleItem = async (req, res) => {
     if (!(await checkIfExists(RaffleItem, id))) {
       return res.status(404).json("Item Does Not Exist!");
     }
-    const { item, description, quantity, logo, resourceLink, rafflePartner } =
+    const { item, description, quantity, resourceLink, rafflePartner } =
       req.body;
+    const logo = req.files?.logo;
     const associatedRafflePartner = await RafflePartner.findOne({
       organization_name: rafflePartner,
     });
+    let logoUrl = null;
+
+    console.log("Cloudinary config:", {
+      name: process.env.CLOUDINARY_CLOUD_NAME,
+      key: process.env.CLOUDINARY_API_KEY,
+      secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    if (logo) {
+      if (businessExists.logo) {
+        const logoUrlParts = businessExists.logo.split("/");
+        const publicIdWithExt = logoUrlParts.slice(-1)[0];
+        const folder = logoUrlParts[logoUrlParts.length - 2];
+        const publicId = `${folder}/${publicIdWithExt.split(".")[0]}`;
+
+        try {
+          await cloudinary.uploader.destroy(publicId);
+          console.log(`Deleted previous logo: ${publicId}`);
+        } catch (err) {
+          console.warn(`Failed to delete previous logo from Cloudinary:`, err);
+        }
+      }
+
+      const dataUri = `data:${logo.mimetype};base64,${logo.data.toString(
+        "base64"
+      )}`;
+      const uploadResult = await cloudinary.uploader.upload(dataUri, {
+        folder: "business_logos",
+        resource_type: "auto",
+      });
+      logoUrl = uploadResult.secure_url;
+    }
 
     const updatedRaffleItem = await RaffleItem.findByIdAndUpdate(
       id,
@@ -45,7 +110,7 @@ const updateRaffleItem = async (req, res) => {
         raffle_partner_name: rafflePartner,
         name: item,
         description: description,
-        image_src: logo,
+        image_src: logoUrl,
         stock: quantity,
         resource_link: resourceLink,
       },
